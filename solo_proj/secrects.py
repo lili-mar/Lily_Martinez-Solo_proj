91d07df22be1a71f6bbb8635700c93160980c7b5
Lily Martinez <lilimar@aol.com>
2021-01-19 18:49:57 -0600

    initial commit

--- solo_proj/secrects.py
+++
@@ -1,2 +0,0 @@
-secret_key_notActive='AIzaSyAxfqupYuXaaffip9SxsB7NReICC6GQtSI'
-second_key='AIzaSyAjWl3sQ7qCYCBGblcwkx7POWFGz0NyWvk'
--- solo_proj/settings.py
+++ solo_proj/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '38s=#*bgt2t0d0b*p534#!pt*2p#1pv^0latx@3h@itwlb%6nm'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY2 = str(os.getenv('SECRET_KEY2'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
